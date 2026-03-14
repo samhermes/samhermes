5776d1e15d75c0eb1aba39ffb6e0b0c26c37a9f3
Sam Hermes <samhermes@me.com>
2026-03-14 16:08:49 -0500

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://samhermes.com',
-    markdown: {
-        shikiConfig: {
-            theme: 'houston',
-        },
-    },
+  site: 'https://samhermes.com',
+
+  markdown: {
+      shikiConfig: {
+          theme: 'houston',
+      },
+  },
+
+  adapter: cloudflare(),
 });
