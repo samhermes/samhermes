681e4ffd09af90e824dbeb07a30d03ebb1762bee
Sam Hermes <samhermes@me.com>
2026-03-22 13:29:26 -0500

    Version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samhermes.com',
-
   markdown: {
       shikiConfig: {
           theme: 'houston',
       },
   },
+  adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
